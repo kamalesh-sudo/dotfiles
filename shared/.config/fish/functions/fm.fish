d188cb313529eac8d33ed8e1a1bcf82492187bf6
kamalesh-sudo <ragavhrxh@gmail.com>
2026-09-24 11:58:56 +0530

    config: initialize path-based dotfiles

--- shared/.config/fish/functions/fm.fish
+++
@@ -1,9 +0,0 @@
-function fm
-    echo -e "\nBefore:"
-    free -h
-    echo -e "\nRestarting swap..."
-    sudo swapoff -a; and sudo swapon -a
-    echo 3 | sudo tee /proc/sys/vm/drop_caches >/dev/null
-    echo -e "\nAfter:"
-    free -h
-end
--- shared/.config/fish/functions/pubip.fish
+++
@@ -1,3 +0,0 @@
-function pubip
-     curl -4 https://api.ipify.org;
-end
